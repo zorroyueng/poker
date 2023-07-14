70e29bf7d8baaab5b912faca4030e17100167f3d
songxg <songxiaoguang@p1.com>
2023-07-14 14:21:45 +0800

    update code

--- lib/demo/poker/poker_tab.dart
+++ lib/demo/poker/poker_tab.dart
@@ -2,8 +2,8 @@ import 'package:base/base.dart';
 import 'package:flutter/material.dart';
 import 'package:poker/base/color_provider.dart';
 import 'package:poker/base/common.dart';
-import 'package:poker/demo/poker/adapter.dart';
 import 'package:poker/demo/demo_helper.dart';
+import 'package:poker/demo/poker/adapter.dart';
 import 'package:poker/poker/config.dart';
 import 'package:poker/poker/logic/poker_adapter.dart';
 import 'package:poker/poker/poker_view.dart';
@@ -22,18 +22,24 @@ class PokerTab extends StatelessWidget {
     bool rotate = false,
   }) =>
       Expanded(
-        child: IconButton(
-          onPressed: onPressed,
-          alignment: Alignment.center,
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.resolveWith((_) => color.withOpacity(.3 * percent)),
-          ),
-          icon: RotatedBox(
-            quarterTurns: rotate ? -2 : 0,
-            child: Icon(
-              icon,
-              color: color.withOpacity(.2 + .8 * percent),
-              size: Common.base(ctx, Config.iconK),
+        child: SizedBox(
+          height: Common.base(ctx, 1.5),
+          child: Common.click(
+            onTap: onPressed,
+            back: Container(
+              decoration: BoxDecoration(
+                color: color.withOpacity(.05 + .3 * percent),
+              ),
+              child: Center(
+                child: RotatedBox(
+                  quarterTurns: rotate ? -2 : 0,
+                  child: Icon(
+                    icon,
+                    color: color.withOpacity(.2 + .8 * percent),
+                    size: Common.base(ctx, Config.iconK),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
@@ -48,65 +54,62 @@ class PokerTab extends StatelessWidget {
             left: 0,
             right: 0,
             bottom: 0,
-            child: Padding(
-              padding: const EdgeInsets.all(0),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  StreamBuilder(
-                    initialData: adapter.percentX().value(),
-                    stream: adapter.percentX().stream().map<double>((v) {
-                      if (v < 0) {
-                        return -v;
-                      } else {
-                        return 0;
-                      }
-                    }).distinct(),
-                    builder: (_, snap) => _btn(
-                      ctx: context,
-                      color: Colors.lime,
-                      icon: Icons.recommend,
-                      onPressed: () => adapter.swipe(SwipeType.left),
-                      percent: snap.data!,
-                      rotate: true,
-                    ),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                StreamBuilder(
+                  initialData: adapter.percentX().value(),
+                  stream: adapter.percentX().stream().map<double>((v) {
+                    if (v < 0) {
+                      return -v;
+                    } else {
+                      return 0;
+                    }
+                  }).distinct(),
+                  builder: (_, snap) => _btn(
+                    ctx: context,
+                    color: Colors.lime,
+                    icon: Icons.recommend,
+                    onPressed: () => adapter.swipe(SwipeType.left),
+                    percent: snap.data!,
+                    rotate: true,
                   ),
-                  StreamBuilder(
-                    initialData: adapter.percentY().value(),
-                    stream: adapter.percentY().stream().map<double>((v) {
-                      if (v < 0) {
-                        return -v;
-                      } else {
-                        return 0;
-                      }
-                    }).distinct(),
-                    builder: (_, snap) => _btn(
-                      ctx: context,
-                      color: Colors.blue,
-                      icon: Icons.stars,
-                      onPressed: () => adapter.swipe(SwipeType.up),
-                      percent: snap.data!,
-                    ),
+                ),
+                StreamBuilder(
+                  initialData: adapter.percentY().value(),
+                  stream: adapter.percentY().stream().map<double>((v) {
+                    if (v < 0) {
+                      return -v;
+                    } else {
+                      return 0;
+                    }
+                  }).distinct(),
+                  builder: (_, snap) => _btn(
+                    ctx: context,
+                    color: Colors.blue,
+                    icon: Icons.stars,
+                    onPressed: () => adapter.swipe(SwipeType.up),
+                    percent: snap.data!,
                   ),
-                  StreamBuilder(
-                    initialData: adapter.percentX().value(),
-                    stream: adapter.percentX().stream().map<double>((v) {
-                      if (v < 0) {
-                        return 0;
-                      } else {
-                        return v;
-                      }
-                    }).distinct(),
-                    builder: (_, snap) => _btn(
-                      ctx: context,
-                      color: Colors.pinkAccent,
-                      icon: Icons.recommend,
-                      onPressed: () => adapter.swipe(SwipeType.right),
-                      percent: snap.data!,
-                    ),
+                ),
+                StreamBuilder(
+                  initialData: adapter.percentX().value(),
+                  stream: adapter.percentX().stream().map<double>((v) {
+                    if (v < 0) {
+                      return 0;
+                    } else {
+                      return v;
+                    }
+                  }).distinct(),
+                  builder: (_, snap) => _btn(
+                    ctx: context,
+                    color: Colors.pinkAccent,
+                    icon: Icons.recommend,
+                    onPressed: () => adapter.swipe(SwipeType.right),
+                    percent: snap.data!,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Positioned(
